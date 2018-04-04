ce773d325daf372722213e459e269de3da9ca908
Fernando Franco Giraldez <ffrancogiraldez@gmail.com>
2018-04-04 20:03:41 +0200

    fix: :art: correct livedata binding.

--- app/src/main/java/es/ffgiraldez/comicsearch/sugestion/ui/SuggestionBindingAdapters.kt
+++ app/src/main/java/es/ffgiraldez/comicsearch/sugestion/ui/SuggestionBindingAdapters.kt
@@ -1,12 +1,7 @@
 package es.ffgiraldez.comicsearch.sugestion.ui
 
-import android.arch.lifecycle.LiveData
-import android.arch.lifecycle.Transformations
 import android.databinding.BindingAdapter
-import android.databinding.BindingConversion
-import android.util.Log
 import com.arlib.floatingsearchview.FloatingSearchView
-import com.arlib.floatingsearchview.suggestions.model.SearchSuggestion
 
 
 @BindingAdapter("on_change")
@@ -14,18 +9,11 @@ fun bindQueryChangeListener(search: FloatingSearchView, listener: FloatingSearch
         search.setOnQueryChangeListener(listener)
 
 @BindingAdapter("suggestions")
-fun <T : SearchSuggestion> bindSuggestions(search: FloatingSearchView, liveData: LiveData<List<T>>) =
-        liveData.observeForever({
-            Log.d("cambio", "tamaño sugestion ${it?.size}")
-            search.swapSuggestions(it)
-        })
+fun bindSuggestions(search: FloatingSearchView, liveData: List<String>) =
+        liveData.map { VolumeSearchSuggestion(it) }.let { search.swapSuggestions(it) }
 
 @BindingAdapter("show_progress")
-fun bindLoading(search: FloatingSearchView, liveData: LiveData<Boolean>) =
-        liveData.observeForever({
-            it?.let { if (it) search.showProgress() else search.hideProgress() }
-        })
-
-@BindingConversion
-fun convertToVolumeSuggestion(input: LiveData<List<String>>): LiveData<List<VolumeSearchSuggestion>> =
-        Transformations.map(input, { value -> value.map { VolumeSearchSuggestion(it) } })
+fun bindLoading(search: FloatingSearchView, liveData: Boolean) = when (liveData) {
+    true -> search.showProgress()
+    false -> search.hideProgress()
+}
