dcf4e5247a2cc520b5e09375f2d0ad0ecb5f1758
Fernando Franco Giraldez <ffrancogiraldez@gmail.com>
2018-04-04 23:51:43 +0200

    chore: log http requests

--- app/src/main/java/es/ffgiraldez/comicsearch/di/ApplicationContext.kt
+++ app/src/main/java/es/ffgiraldez/comicsearch/di/ApplicationContext.kt
@@ -4,6 +4,8 @@ import es.ffgiraldez.comicsearch.comics.ComicRepository
 import es.ffgiraldez.comicsearch.comics.data.ComicVineApi
 import es.ffgiraldez.comicsearch.search.presentation.SearchViewModel
 import es.ffgiraldez.comicsearch.sugestion.presentation.SuggestionViewModel
+import okhttp3.OkHttpClient
+import okhttp3.logging.HttpLoggingInterceptor
 import org.koin.dsl.module.applicationContext
 import retrofit2.Retrofit
 import retrofit2.adapter.rxjava2.RxJava2CallAdapterFactory
@@ -12,10 +14,16 @@ import retrofit2.converter.gson.GsonConverterFactory
 
 val comicContext = applicationContext {
     bean {
+        val okHttp = OkHttpClient.Builder()
+                .addInterceptor(HttpLoggingInterceptor()
+                        .apply { level = HttpLoggingInterceptor.Level.BASIC }
+                )
+
         Retrofit.Builder()
                 .addConverterFactory(GsonConverterFactory.create())
                 .addCallAdapterFactory(RxJava2CallAdapterFactory.create())
                 .baseUrl(ComicVineApi.BASE_URL)
+                .client(okHttp.build())
                 .build()
                 .create(ComicVineApi::class.java)
     }
